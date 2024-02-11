8ef7b0bed5886cd2f39923c105b93bb129c9a006
Kim Gross <kgross@jensalt.com>
2024-02-11 09:45:12 -0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,15 @@
+/* This is a sample program written by
+ * kim.gross@malad.us
+ * Just a sample hello world for the students. 
+ * 2-11-24
+ */
+
+
+
+
 public class helloworld {
     public static void main (String[] args){
-        System.out.println("hello world how you are today");
+        System.out.println("hello world how you are today");// printing out hello world for all the world to see. 
     }
 
 }
